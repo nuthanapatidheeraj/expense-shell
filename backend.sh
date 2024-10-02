0f380e1037fd7672be0d005a1c612b9f2614bb0c
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-10-02 19:16:06 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -52,4 +52,15 @@ then
     VALIDATE $? "Creating expense user"
 else 
     echo -e "expense user already exists...$Y SKIPPING $N" 
-fi       
+fi   
+
+
+mkdir /app
+VALIDATE $? "Creating /app folder"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend application code"
+
+cd /app
+unzip /tmp/backend.zip
+VALIDATE $? "Extracting backend application code"
