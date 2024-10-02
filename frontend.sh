5e8b19800634d47b3fae0f24916d18f32090502d
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-10-02 20:18:07 +0530

    shell

--- frontend.sh
+++ frontend.sh
@@ -55,5 +55,7 @@ unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extract frontend code"
 
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "copied expense conf"
 
-systemctl restart nginx
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "restarted nginx"
